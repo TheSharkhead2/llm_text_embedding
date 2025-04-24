a8a4d7d0ed49ad21e3171b07c377c45f913383b4
Theo Rode <theorodester@gmail.com>
2025-04-24 09:41:09 -0700

    nix shell

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  name = "dev-shell";
+
+  # buildInputs = with pkgs; [ jupyter ];
+
+  packages = [
+    (pkgs.python312.withPackages
+      (ps: [ ps.numpy ps.python-lsp-server ps.matplotlib ps.scikitlearn ]))
+  ];
+}
+
