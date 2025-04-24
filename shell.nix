a4c01a5300ad06b13f3903e7d302b50763d5aafd
Theo Rode <theorodester@gmail.com>
2025-04-24 09:44:31 -0700

    kaggle

--- shell.nix
+++ shell.nix
@@ -6,8 +6,13 @@ pkgs.mkShell {
   # buildInputs = with pkgs; [ jupyter ];
 
   packages = [
-    (pkgs.python312.withPackages
-      (ps: [ ps.numpy ps.python-lsp-server ps.matplotlib ps.scikitlearn ]))
+    (pkgs.python312.withPackages (ps: [
+      ps.numpy
+      ps.python-lsp-server
+      ps.matplotlib
+      ps.scikitlearn
+      ps.kaggle
+    ]))
   ];
 }
 
